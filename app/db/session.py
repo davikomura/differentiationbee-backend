d6e1364ac14c048ea40cea040187378c656e7416
Davi Komura de Castro <davi.komura@gmail.com>
2025-08-04 09:06:53 +0100

    config render

--- app/db/session.py
+++ app/db/session.py
@@ -7,9 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
